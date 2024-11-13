5eef299b989eb699bf3ef387bc73278368d90067
Santosmell21 <santos.mell@escola.pr.gov.br>
2024-11-13 08:41:33 -0300

    13/11

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(500, 500);
+    }
+    
+    function draw() {
+      background("(79,79,79);
+                 
+       noStroke(); //função para repetir a borda
+      
+      if (mouseIsPressed) {
+        fill("(0,255,255);
+             
+            rect(0, 0, 100,);
+      } else {
+        fill("(124,252,0);
+             
+             
